aa83e279b66ee376a58082018102a6f4bde10460
Fernando Ortega <feortegas@gmail.com>
2022-02-11 12:34:27 -0600

    add Thought, Reaction and Friend models

---
+++ models/Thought.js
@@ -0,0 +1,69 @@
+const { Schema, model } = require('mongoose');
+
+// define Schema for Thought creation
+const ThoughtSchema = new Schema(
+    {
+        writtenBy: {
+            type: String,
+            required: true
+        },
+        commentBody: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        },
+        // use ReplySchema to validate data for a reply
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+
+// define Schema for Reaction creation
+const ReactionSchema = new Schema(
+    {
+
+        // set custom id to avoid confusion with parent comment _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true
+        },
+        writtenBy: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+CommentSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+// create the Thought model using UserSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -20,9 +20,27 @@ const UserSchema = new Schema(
                 type: Schema.Types.ObjectId,
                 ref: 'Thought'
             }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Friend'
+            }
         ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
 );
+
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
 // create the User model using UserSchema
 const User = model('User', UserSchema);
